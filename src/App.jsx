f9cb0a5bf430d9435baa9628f16722d1f713d99b
ulus <uluskk@gmail.com>
2024-05-01 15:53:49 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import Navbar from "./components/navbar/Navbar";
 import HomePage from "./routes/homePage/homePage";
+import ListPage from "./routes/listPage/listPage";
 import "./layout.scss";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+  ]);
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        {" "}
-        <HomePage />
-      </div>
-    </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     {" "}
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/routes/listPage/listPage.jsx
@@ -0,0 +1,7 @@
+import "./listPage.scss";
+
+function ListPage() {
+  return <div className="listPage">listPage</div>;
+}
+
+export default ListPage;
